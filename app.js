5861f1d13eba94ffc79dae76a962cd9e0b2d1e38
priyatam0509 <piyushpriya34@gmail.com>
2025-05-28 21:44:30 +0530

    2nd

--- app.js
+++ app.js
@@ -105,6 +105,12 @@ app.post('/completeUpload', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
+
